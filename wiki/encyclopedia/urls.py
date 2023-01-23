55b7fb370ae1b77d72e75838dfc100c17c531d48
Sarr Loum <sarrloum10@gmail.com>
2023-01-23 23:25:53 +0100

    add convert_md_to_html function

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("entry", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("newpage", views.newpage, name="newpage"),
     path("random", views.random, name="random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,23 @@
+from markdown2 import Markdown
 from django.shortcuts import render
 
 from . import util
+
+
+# Function that converts the content of a given page title from markdown to html
+def convert_md_to_html(title):
+
+    # Get the content using get_entry function from util.py
+    content = util.get_entry(title)
+    markdowner = Markdown()
+
+    # check if file exist/or not and return the content in html 
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+
 
 
 def index(request):
@@ -8,13 +25,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return render(request, "encyclopedia/entry.html", {
-    })
+# Function that render entries
+def entry(request,):
 
+    return render(request, "encyclopedia/entry.html")
+
+# Function that render create new page
 def newpage(request):
     return render(request, "encyclopedia/entry.html", {
     })
 
+# Function that randomly render entries
 def random(request):
     return render(request, "encyclopedia/random.html")
