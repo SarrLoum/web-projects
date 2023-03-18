874e1a4b3974dee9d5e4eb049beec4c171ba3a7f
Sarr Loum <sarrloum10@gmail.com>
2023-03-18 01:55:32 +0100

    added retrieve thred

--- project4/network/api_views.py
+++ project4/network/api_views.py
@@ -2,7 +2,10 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from django.db.models import Q
+from django.http import Http404
+
 
 
 from .models import *
