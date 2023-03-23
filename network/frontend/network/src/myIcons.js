e9ad37f54997205ab6823e492f7d8d2f1a897dad
Sarr Loum <sarrloum10@gmail.com>
2023-03-23 16:51:17 +0000

    icon components done

---
+++ network/frontend/network/src/myIcons.js
@@ -0,0 +1,32 @@
+import bell-notification from ".icons/bell-notification.svg";
+import bookmark from ".icons/bookmark.svg"
+import cube from ".icons/cube.svg"
+import hash-explore from ".icons/hash-explore.svg"
+import home from ".icons/home.svg"
+import list from ".icons/list.svg"
+import more from ".icons/more.svg"
+import more2 from ".icons/more2.svg"
+import paper-plane from ".icons/paper-plane.svg"
+import person from ".icons/person.svg"
+import "./myIcons.css";
+
+
+const Notification = () => <img src={bell-notification}></img>
+
+const Bookmark = () => <img src={bookmark}></img>
+
+const Cube = () => <img src={cube}></img>
+
+const HashExplore = () => <img src={hash-explore}></img>
+
+const Home = () => <img src={home}></img>
+
+const List = () => <img src={list}></img>
+
+const More = () => <img src={more}></img>
+
+const More2 = () => <img src={more2}></img>
+
+const PaperPlane = () => <img src={paper-plane}></img>
+
+const Person = () => <img src={person}></img>
